2296090ea22fbb89ef6bfc9a8af384634148654d
timgall <119544130+timgall@users.noreply.github.com>
2023-05-11 15:12:08 -0500

    updated fetches

--- public/app.js
+++ public/app.js
@@ -3,3 +3,27 @@ fetch("/api/users")
   .then((data) => {
     console.log(data);
   });
+
+fetch("/api/bbqforum")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  });
+
+fetch("/api/bourbonforum")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  });
+
+fetch("/api/bbqrecipes")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  });
+
+fetch("/api/bourbonreviews")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  });
