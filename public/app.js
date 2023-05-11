731c04d02802bbee80d14957d4469fc19da2e959
timgall <119544130+timgall@users.noreply.github.com>
2023-05-11 17:35:32 -0500

    username input

--- public/app.js
+++ public/app.js
@@ -1,7 +1,47 @@
+const searchBtn = $(".search");
+
+const users = $(".users");
+const bbqforum = $(".bbqforum");
+const bbqrecipes = $(".bbqforum");
+const bourbonforum = $(".bourbonforum");
+const bourbonreviews = $(".bourbonreviews");
+
+const userName = prompt("Please Enter Your User Name.");
+console.log(userName);
 fetch("/api/users")
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
+
+    let userExists = false;
+    data.forEach((user) => {
+      if (user.user_name === userName) {
+        userExists = true;
+        let passwordInput;
+        let passwordAttempts = 0;
+        let passwordAccurate = false;
+        while (!passwordAccurate && passwordAttempts < 3) {
+          passwordInput = prompt(
+            `Welcome ${userName}. Please enter your password.`
+          );
+          passwordAttempts++;
+          if (user.user_password === passwordInput) {
+            passwordAccurate = true;
+            alert(`Welcome ${userName}!`);
+            // set visibility of homescreen to welcome screen
+          } else if (passwordAttempts === 3) {
+            alert("You have exceeded the maximum number of attempts. Goodbye.");
+            window.close();
+          } else {
+            alert("Password was incorrect. Please try again.");
+          }
+        }
+      }
+    });
+    if (!userExists) {
+      alert(`User ${userName} does not exist!`);
+      $(".newUser").css("display", "block");
+    }
   });
 
 fetch("/api/bbqforum")
